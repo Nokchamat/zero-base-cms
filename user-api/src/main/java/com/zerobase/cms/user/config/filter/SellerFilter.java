142153e7d3131760b98a92e6ea34c26c39a5f64c
NokchaMat <rkdtjd976@gmail.com>
2023-05-08 16:58:49 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현.

---
+++ user-api/src/main/java/com/zerobase/cms/user/config/filter/SellerFilter.java
@@ -0,0 +1,36 @@
+package com.zerobase.cms.user.config.filter;
+
+import com.zerobase.cms.domain.user.config.JwtAuthenticationProvider;
+import com.zerobase.cms.domain.user.domain.common.UserVo;
+import com.zerobase.cms.user.service.seller.SellerService;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/seller/*")
+@RequiredArgsConstructor
+public class SellerFilter implements Filter {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final SellerService sellerService;
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String token = req.getHeader("X-AUTH-TOKEN");
+        if (!jwtAuthenticationProvider.validationToken(token)) {
+            throw new ServletException("Invalid Access");
+        }
+
+        UserVo vo = jwtAuthenticationProvider.getUserVo(token);
+        sellerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
+                () -> new ServletException("Invalid access")
+        );
+        chain.doFilter(request, response);
+
+    }
+}
