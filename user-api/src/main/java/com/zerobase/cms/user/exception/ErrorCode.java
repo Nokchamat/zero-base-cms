bce3e8df9ca16fbfc43a14972c5a35c5fd8b7a55
NokchaMat <rkdtjd976@gmail.com>
2023-05-08 16:33:12 +0900

    feat : [005-SELLER-SIGN-UP-IN] 셀러에 대한 사인 업, 사인 인 구현 seller/* 에 대한 token filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
+++ user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
@@ -15,10 +15,14 @@ public enum ErrorCode {
     EXPIRE_CODE(HttpStatus.BAD_REQUEST, "인증 시간이 만료되었습니다."),
     NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "일치하는 회원이 없습니다."),
 
+    HERE_IN_ERROR(HttpStatus.BAD_REQUEST, "여기가 에러입니다만"),
+
     //login
-    LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 확인해 주세요.");
+    LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 확인해 주세요."),
 
+    NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다.")
 
+;
     private final HttpStatus httpStatus;
     private final String detail;
 }
